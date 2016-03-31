d1aff519b52e17fd6c5f85d7bee20e3b06b8e9b6
lateminer <late-miner@ya.ru>
2016-04-01 00:16:12 +0300

    Set EXPLORE_TRANSACTION_PATH instead of "tx/"

--- wallet/src/de/schildbach/wallet/ui/WalletTransactionsFragment.java
+++ wallet/src/de/schildbach/wallet/ui/WalletTransactionsFragment.java
@@ -313,7 +313,7 @@ public class WalletTransactionsFragment extends Fragment implements LoaderCallba
 
 					case R.id.wallet_transactions_context_browse:
 						if (!txRotation)
-							startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(Constants.EXPLORE_BASE_URL + "tx/" + tx.getHashAsString())));
+							startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(Constants.EXPLORE_BASE_URL + Constants.EXPLORE_TRANSACTION_PATH + tx.getHashAsString())));
 						else
 							startActivity(new Intent(Intent.ACTION_VIEW, KEY_ROTATION_URI));
 						return true;
